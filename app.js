5aaacc1dcd04bbba94672b2e6a3a2740df44ba6c
Meesum Ali Shahid <156599701+Meesum2004@users.noreply.github.com>
2024-03-04 11:10:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let gameNum = 25;
+let userNum = prompt("Guess Enter the Right Number");
+
+while (gameNum != userNum) {
+  userNum = prompt("You Enter The Wrong Number ! Guess Again");
+}
+alert("Congrulation! You Enter  the Right Number!");
